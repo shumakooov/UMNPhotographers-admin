c03670e1670578d02d80ea3689609ed5b9265f87
shumakooov <shumakov.13@yandex.ru>
2023-11-07 23:38:58 +0500

    для деплоя

--- src/index.tsx
+++ src/index.tsx
@@ -12,9 +12,9 @@ const root = ReactDOM.createRoot(
 );
 root.render(
     <React.StrictMode>
-        <HashRouter>
+        <BrowserRouter>
             <App/>
-        </HashRouter>
+        </BrowserRouter>
     </React.StrictMode>
 );
 
