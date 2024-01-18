f1a757e9592f0e15101808c04c6624ef743b124f
stlvce <egorglv@mail.ru>
2024-01-18 18:02:43 +0300

    env

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: `${process.env.REACT_APP_API_URL}/admin`,
+  baseURL: "https://photographersekb.ru:8080/admin",
   withCredentials: true,
 });
 
@@ -16,7 +16,7 @@ instance.interceptors.response.use(
     } else {
       return Promise.reject(error);
     }
-  }
+  },
 );
 
 export default instance;
